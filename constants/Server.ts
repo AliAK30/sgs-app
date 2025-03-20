b546747d6d7580799e2ccfd6f2ffd26445ef368b
Ali Ahmed Khan <aliahmed.k30062@gmail.com>
2025-03-21 01:54:14 +0500

    fixed

--- constants/Server.ts
+++ constants/Server.ts
@@ -1,4 +1,4 @@
-export const url: string = "http://edumatch.southeastasia.cloudapp.azure.com";
+export const url: string = "https://edumatch.southeastasia.cloudapp.azure.com";
 
 
 //http://192.168.0.100:8080
