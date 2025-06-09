013318b222ee016ec09f4cfece0a82d6980f3722
pizzalaylo <ateeq800@gmail.com>
2025-06-09 15:03:37 +0500

    fixed

--- app/(student)/index.tsx
+++ app/(student)/index.tsx
@@ -10,8 +10,7 @@ import { useRouter } from "expo-router";
 import { LinearGradient } from "expo-linear-gradient";
 import SimilarStudents from "@/components/SimilarStudents";
 import { formatTwoWordsName } from "@/utils";
-import * as Haptics from 'expo-haptics';
-import { triggerVibration } from "@/constants/Vibrations";
+import * as Haptics from '@/components/Haptics';
 import Animated, {
     useSharedValue,
     useAnimatedStyle,
